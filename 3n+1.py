e270f91a104c063705b76a8855d02a89362780de
Tejas Tribhuvan <114176583+TEJASTRIBHUVAN200223@users.noreply.github.com>
2024-05-13 18:12:54 +0530

    Update 3n+1.py
    Previous Code was not Accpeted in the Judge que on online judge

--- 3n+1.py
+++ 3n+1.py
@@ -1,11 +1,35 @@
-def collatz(n):
-    while n != 1:
-        print(n)
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = (n * 3) + 1
-    print(n)
+'''
+20180113	jlhung	v1.0
+'''
 
-# Example usage
-collatz(6)
+a = {}
+
+def cal(x):
+	if x in a:
+		return a[x]
+	
+	if x <= 1:
+		return 1
+	
+	if x % 2 == 1:
+		y = 3 * x + 1
+	else:
+		y = x // 2
+	
+	a[x] = cal(y) + 1
+	return a[x]
+
+while True:
+	try:
+		x, y = map(int, input().split())
+	except EOFError:
+		break
+
+	max_cycle = 0
+	for i in range(min(x, y), max(x, y) + 1):
+		n = cal(i)
+			
+		if n > max_cycle:
+			max_cycle = n
+	
+	print(x, y, max_cycle)
